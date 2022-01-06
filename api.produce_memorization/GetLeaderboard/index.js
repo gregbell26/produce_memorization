667f5ff5f8a0a1cf82190d554b54a226f998be5a
Gregory Bell <trihard.studios@outlook.com>
2022-01-06 13:22:55 -0700

    Added more functions; updated function configs

--- api.produce_memorization/GetLeaderboard/index.js
+++ api.produce_memorization/GetLeaderboard/index.js
@@ -1,13 +1,23 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const CosmosClient = require("@azure/cosmos").CosmosClient
+const endpoint = process.env.CosmosDBEndpoint;
+const apiKey = process.env.CosmosAPIKey;
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+module.exports = async function (context, req) {
+    //todo verify request
+
+    const client = new CosmosClient({ endpoint: endpoint, key: apiKey });
+    const database = client.database("produce-memorization");
+    const leaderBoardContainer = database.container("Leaderboard");
+
+    const getLeaderBoard = "SELECT Leaderboard.leaderboard FROM Leaderboard WHERE Leaderboard.id='top-5'";
+    const item = await leaderBoardContainer.items
+        .query({query: getLeaderBoard})
+        .fetchAll();
+
+    //todo add data checks
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: item.resources[0]
     };
 }
--- api.produce_memorization/PutLeaderboard/index.js
+++ api.produce_memorization/PutLeaderboard/index.js
@@ -1,13 +1,43 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const CosmosClient = require("@azure/cosmos").CosmosClient
+const https = require('https')
+const endpoint = process.env.CosmosDBEndpoint;
+const apiKey = process.env.CosmosAPIKey;
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+async function getCurrentLeaderboard(_endpoint, _apiKey){
+    const authenticatedEndpoint = _endpoint + "?code=" + _apiKey;
+    // add error handling
+    return new Promise(resolve => {
+        let data = "";
+        https.get(authenticatedEndpoint, res => {
+            res.on('data', (chunk) =>  (data += chunk));
+            res.on('end', () => resolve(data));
+        })
+    })
+}
+
+
+module.exports = async function (context, req) {
+    const client = new CosmosClient({ endpoint: endpoint, key: apiKey });
+    const database = client.database("produce-memorization");
+    const leaderBoardContainer = database.container("Leaderboard");
+
+    const data = req.body.data;
+    const curLeaderboard = await getCurrentLeaderboard(process.env.GetLeaderboardEndpoint, process.env.GetLeaderboardKey);
+    let leaderboard = JSON.parse(curLeaderboard).leaderboard;
+
+    // We are adding data to the array, then sorting, then chopping the lowest lad off.
+    leaderboard.push(data)
+
+    leaderboard.sort((a,b) => {
+        return ((a['points'] > b['points']) ? -1 : ((a['points'] < b['points']) ? 1 : 0));
+    })
+
+    leaderboard.splice(5);
+
+    await leaderBoardContainer.item("top-5")
+        .replace({id: "top-5", "leaderboard": leaderboard});
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
     };
 }
